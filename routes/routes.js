14abe67b09132ad654cb7bbb980add7a30bc5d8a
Jamel Gooding <jgo05@sbgml02397.sbg.skybettingandgaming.com>
2018-09-14 15:34:37 +0100

    Added index.html and scorecards.html

--- routes/routes.js
+++ routes/routes.js
@@ -5,14 +5,18 @@ var path = require("path");
 
 var router = function(app) {
     //put all the routes
-    app.route("/login").get(function(req,res) {
+    app.route("/index").get(function(req, res) {
+        res.sendFile(path.resolve('views/index.html'));
+    });
+    app.route("/login").get(function(req, res) {
         res.sendFile(path.resolve('views/login.html'));
     });
-    app.route("/registration").get(function(req,res) {
+    app.route("/registration").get(function(req, res) {
         res.sendFile(path.resolve('views/registration.html'));
     });
-    
-    
+    app.route("/scorecards").get(function(req, res) {
+        res.sendFile(path.resolve('views/scorecards.html'));
+    });
 };
 
 module.exports = router;
