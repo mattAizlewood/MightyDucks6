3d0b1b337b9fabcc5accfe670c185ffbfc98ff6d
cc190 <cch01@sbgml02919.sbg.skybettingandgaming.com>
2018-09-20 14:17:01 +0100

    Login and Register functionality

--- routes/routes.js
+++ routes/routes.js
@@ -3,20 +3,13 @@ var router = express.Router();
 var fs = require('fs');
 var path = require("path");
 var userControllers = require('../controllers/userController.js');
-var leaderboardControllers = require('../controllers/leaderboardController.js');
 
 var router = function(app) {
-    //put all the routes
-    app.route("/login").get(function(req,res) {
-        res.sendFile(path.resolve('views/login.html'));
-    });
-    app.route("/registration").get(function(req,res) {
-        res.sendFile(path.resolve('views/registration.html'));
-    });
+    app.route('/login')
+    .post((req,res) => userControllers.viewSingle(app,req,res))
     
-    app.route('/api/getAllLeaderboards').get((req,res) => {
-        leaderboardControllers.getAllLeaderboards(app,req,res);
-    });
+    app.route('/registration')
+    .post((req,res) => userControllers.register(app,req,res))
     
 };
 
