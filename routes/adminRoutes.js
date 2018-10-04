e98439bbd1e85c393a529ae0d45b3812df9d8f2e
Matthew Aizlewood <matthew.aizlewood.skybettingandgaming.com>
2018-10-04 12:22:34 +0100

    added view all scorecards and results and the api to delete individual scorecard and results too

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,11 +3,28 @@ var path = require("path");
 var userControllers = require('../controllers/userController.js');
 var leaderboardControllers = require('../controllers/leaderboardController.js');
 var calculateScoreControllers = require('../controllers/calculateScoresController');
+var adminControllers = require('../controllers/adminController');
 
 var router = function(app) {
 
     app.route('/admin/settings').get((req,res) => {
         res.sendFile(path.resolve('views/settings.html'));
+    });
+
+    app.route('/api/getAllScoreCards').get((req, res) => {
+        adminControllers.getAllScoreCards(app,req,res);
+    });
+
+    app.route('/api/getAllScoreCardResults').get((req, res) => {
+        adminControllers.getAllScoreCardResults(app,req,res);
+    });
+
+    app.route('/api/deleteScoreCard').delete((req,res) => {
+        adminControllers.deleteScoreCard(app,req,res);
+    });
+
+    app.route('/api/deleteScoreCardResult').delete((req,res) => {
+        adminControllers.deleteScoreCardResult(app,req,res);
     });
     
 };
