8133bf3d55c237161da3c550fbee0cb5581bb61a
Jamel <jamel.gooding@skybettingandgaming.com>
2018-10-04 11:31:07 +0100

    Added Mobile CSS
    Index/Predictions.html

--- views/js/populateScorecard.js
+++ views/js/populateScorecard.js
@@ -32,27 +32,27 @@
             }
 
             //Match 1
-            document.getElementById('team_1').src = "../img/" + scorecardData[0].roundInfo.match1.homeTeam + ".png"
-            document.getElementById('team_2').src = "../img/" + scorecardData[0].roundInfo.match1.awayTeam + ".png"
+            document.getElementById('home_1').src = "../img/" + scorecardData[0].roundInfo.match1.homeTeam + ".png"
+            document.getElementById('away_1').src = "../img/" + scorecardData[0].roundInfo.match1.awayTeam + ".png"
 
             // //Match 2
-            document.getElementById('team_3').src = "../img/" + scorecardData[0].roundInfo.match2.homeTeam + ".png"
-            document.getElementById('team_4').src = "../img/" + scorecardData[0].roundInfo.match2.awayTeam + ".png"
+            document.getElementById('home_2').src = "../img/" + scorecardData[0].roundInfo.match2.homeTeam + ".png"
+            document.getElementById('away_2').src = "../img/" + scorecardData[0].roundInfo.match2.awayTeam + ".png"
 
             // //Match 3
-            document.getElementById('team_5').src = "../img/" + scorecardData[0].roundInfo.match3.homeTeam + ".png"
-            document.getElementById('team_6').src = "../img/" + scorecardData[0].roundInfo.match3.awayTeam + ".png"
+            document.getElementById('home_3').src = "../img/" + scorecardData[0].roundInfo.match3.homeTeam + ".png"
+            document.getElementById('away_3').src = "../img/" + scorecardData[0].roundInfo.match3.awayTeam + ".png"
 
             // //Match 4
-            document.getElementById('team_7').src = "../img/" + scorecardData[0].roundInfo.match4.homeTeam + ".png"
-            document.getElementById('team_8').src = "../img/" + scorecardData[0].roundInfo.match4.awayTeam + ".png"
+            document.getElementById('home_4').src = "../img/" + scorecardData[0].roundInfo.match4.homeTeam + ".png"
+            document.getElementById('away_4').src = "../img/" + scorecardData[0].roundInfo.match4.awayTeam + ".png"
 
             // //Match 5
-            document.getElementById('team_9').src = "../img/" + scorecardData[0].roundInfo.match5.homeTeam + ".png"
-            document.getElementById('team_10').src = "../img/" + scorecardData[0].roundInfo.match5.awayTeam + ".png"
+            document.getElementById('home_5').src = "../img/" + scorecardData[0].roundInfo.match5.homeTeam + ".png"
+            document.getElementById('away_5').src = "../img/" + scorecardData[0].roundInfo.match5.awayTeam + ".png"
 
             // //Match 6
-            document.getElementById('team_11').src = "../img/" + scorecardData[0].roundInfo.match6.homeTeam + ".png"
-            document.getElementById('team_12').src = "../img/" + scorecardData[0].roundInfo.match6.awayTeam + ".png"
+            document.getElementById('home_6').src = "../img/" + scorecardData[0].roundInfo.match6.homeTeam + ".png"
+            document.getElementById('away_6').src = "../img/" + scorecardData[0].roundInfo.match6.awayTeam + ".png"
         });
 })();
